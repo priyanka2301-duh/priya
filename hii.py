8a205a989761061fbe88e90fb7a3a6b26f80dffe
priyanka2301-duh <84245880+priyanka2301-duh@users.noreply.github.com>
2021-05-16 03:06:42 -0700

    Add files via upload

---
+++ hii.py
@@ -0,0 +1,8 @@
+Python 2.7.10 (default, May 23 2015, 09:44:00) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("hiiiiii')
+       
+SyntaxError: EOL while scanning string literal
+>>> print("hiii")
+hiii
+>>> 
